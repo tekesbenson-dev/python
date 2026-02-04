0a914b71fdd74429d68f90df90aef028aaaac43c
tekesbenson <tekesbenson@gmail.com>
2026-02-04 14:02:35 +0300

    add updated files

--- lesson2.py
+++ lesson2.py
@@ -4,3 +4,39 @@ number1= 10
 number2= 20
 addition = number1 + number2
 print("the sum of the two numbers is:", addition)
+
+
+#subtraction
+number3=50
+number4=15
+difference = number3 - number4
+print("the difference of the number is:", difference)
+
+#multiplicatin
+number5=10
+number6=6
+product= number5 * number6
+print("The product of the number is:", product)
+
+#division
+number7=56
+number8=7
+division= number7 / number8
+print("the obleus of the number is:", division)
+
+#modulus - this means the remainder of a  number subdivide by another one
+number9=14
+number10=5
+modulus = number9 % number10
+print("the modulus of the number is:", modulus)
+
+#floor division - is the number of times a number subdivides a  number ignoring the remainder
+floor=number9 // number10
+print("The floor division of the two number is:", floor)
+
+
+#exponent(**)
+#the power of a number
+number11=10
+exponent= number11 ** 2
+print("The ecponent of the  number is:", exponent)
