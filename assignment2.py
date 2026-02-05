c9d4fd35a89024b649951c3c1aaa036fb97604ba
tekesbenson <tekesbenson@gmail.com>
2026-02-05 22:41:18 +0300

    add updated files

---
+++ assignment2.py
@@ -0,0 +1,8 @@
+# Create a tuple of 8 planets
+planets = ("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+
+# Print all planets
+print("All planets:", planets)
+
+# Print planets from index 2 to 6 (index 6 is not included)
+print("Planets from index 2 to 6:", planets[2:6])
